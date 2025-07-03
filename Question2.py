0766f38b4ea1d2086e4246bf5752dc20c91bcb7f
Keerthan K V <keerthan11c@gmail.com>
2025-07-03 23:36:10 +0530

    Update Question2.py

--- Question2.py
+++ Question2.py
@@ -13,24 +13,25 @@ from sklearn.impute import SimpleImputer
 from sklearn.preprocessing import StandardScaler
 
 iris=load_iris()
-df=pd.DataFrame(
-    np.c_[iris.data,iris.target],
-    columns=iris.feature_names+['target']
-)
+# df=pd.DataFrame(
+#     np.c_[iris.data,iris.target],
+#     columns=iris.feature_names+['target']
+# )
 
-df.dropna(subset=['target'],inplace=True)
-f_cols=iris.feature_names
-df[f_cols]=df[f_cols].replace(0,np.nan)
-X=df.drop(['target'],axis=1)
-y=df['target']
+# df.dropna(subset=['target'],inplace=True)
+# f_cols=iris.feature_names
+# df[f_cols]=df[f_cols].replace(0,np.nan)
 
-imputer= SimpleImputer(missing_values=np.nan, strategy='median')
-X_imputed=imputer.fit_transform(X)
-X_df_=pd.DataFrame(X_imputed,columns=X.columns)
-scaler=StandardScaler()
-X_df=scaler.fit_transform(X_df_)
+X=iris.data
+y=iris.target
 
-X_train,X_test,y_train,y_test=train_test_split(X_df,y,test_size=0.2, random_state=42)
+# imputer= SimpleImputer(missing_values=np.nan, strategy='median')
+# X_imputed=imputer.fit_transform(X)
+# X_df_=pd.DataFrame(X_imputed,columns=X.columns)
+# scaler=StandardScaler()
+# X_df=scaler.fit_transform(X_df_)
+
+X_train,X_test,y_train,y_test=train_test_split(X,y,test_size=0.2, random_state=42)
 
 id3 = DecisionTreeClassifier(criterion="entropy")
 id3.fit(X_train, y_train)
