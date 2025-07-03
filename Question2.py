577bd9d80898b2eb6a6aa74e72801ab3514fb36d
Keerthan K V <keerthan11c@gmail.com>
2025-07-03 09:14:32 +0530

    Update Question2.py

--- Question2.py
+++ Question2.py
@@ -3,31 +3,42 @@
 # sample. Interpret the results. Write the inference/analysis of each output.
 
 from sklearn.datasets import load_iris
-from sklearn.tree import DecisionTreeClassifier, plot_tree
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import accuracy_score
+from sklearn.tree import DecisionTreeClassifier,plot_tree
+from sklearn.metrics import *
 import matplotlib.pyplot as plt
+import numpy as np
+from sklearn.model_selection import train_test_split
+import pandas as pd
+from sklearn.impute import SimpleImputer
+from sklearn.preprocessing import StandardScaler
 
+iris=load_iris()
+df=pd.DataFrame(
+    np.c_[iris.data,iris.target],
+    columns=iris.feature_names+['target']
+)
 
-iris = load_iris()
-X, y = iris.data, iris.target
-feature_names, target_names = iris.feature_names, iris.target_names
+df.dropna(subset=['target'],inplace=True)
+f_cols=iris.feature_names
+df[f_cols]=df[f_cols].replace(0,np.nan)
+X=df.drop(['target'],axis=1)
+y=df['target']
 
+imputer= SimpleImputer(missing_values=np.nan, strategy='median')
+X_imputed=imputer.fit_transform(X)
+X_df_=pd.DataFrame(X_imputed,columns=X.columns)
+scaler=StandardScaler()
+X_df=scaler.fit_transform(X_df_)
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=0)
+X_train,X_test,y_train,y_test=train_test_split(X_df,y,test_size=0.2, random_state=42)
 
-model = DecisionTreeClassifier(criterion='entropy')
-model.fit(X_train, y_train)
+id3 = DecisionTreeClassifier(criterion="entropy")
+id3.fit(X_train, y_train)
 
-y_pred = model.predict(X_test)
-acc = accuracy_score(y_test, y_pred)
-print(f"Accuracy on test set: {acc:.2f}")
+y_pred = id3.predict(X_test)
+accuracy = accuracy_score(y_test, y_pred)
+print(f"Accuracy: {accuracy}")
 
-sample = [[5.1, 3.5, 1.4, 0.2]]
-pred = model.predict(sample)[0]
-print("Predicted class for sample:", target_names[pred])
-
-plt.figure(figsize=(10, 6))
-plot_tree(model, feature_names=feature_names, class_names=target_names, filled=True)
-plt.title("Decision Tree (ID3 - Iris Dataset)")
+plt.figure(figsize=(12, 8))
+plot_tree(id3, filled=True, feature_names=iris.feature_names, class_names=iris.target_names, rounded=True, proportion=True)
 plt.show()
